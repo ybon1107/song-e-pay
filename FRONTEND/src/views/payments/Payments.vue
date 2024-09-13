7c0104c278977cda638e0c4890260045380d7a4f
ssooomm <fivesoomin@gmail.com>
2024-09-13 17:55:22 +0900

    버섯 사진 추가

--- FRONTEND/src/views/payments/Payments.vue
+++ FRONTEND/src/views/payments/Payments.vue
@@ -1,6 +1,14 @@
 
 <template>
-  <div class="py-4 container-fluid">
-    <h2>결제</h2>
+  <div class="container">
+    <h1>hi</h1>
+    <div class="row mt-lg-n10 mt-md-n11 mt-n10 justify-content-center">
+      <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
+        <div>
+          <!-- <h2>hi</h2> -->
+          <!-- <img src="../../assets/img/mushroom.png" class="p-3" alt="mushroom" /> -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
