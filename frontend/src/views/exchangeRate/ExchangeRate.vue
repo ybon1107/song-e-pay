377cad3d174f548555bdd6921a69888e48fac9d3
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-09-20 17:42:02 +0900

    Revert "환경변수로 변경"

--- frontend/src/views/exchangeRate/ExchangeRate.vue
+++ frontend/src/views/exchangeRate/ExchangeRate.vue
@@ -13,12 +13,22 @@
               <canvas id="exchangeRateChart"></canvas>
             </div>
             <div class="input-group my-3">
-              <input type="number" class="form-control" v-model.number="usdAmount" @input="convertToKrw" />
+              <input
+                type="number"
+                class="form-control"
+                v-model.number="usdAmount"
+                @input="convertToKrw"
+              />
               <div class="input-group-append">
                 <span class="input-group-text">USD</span>
               </div>
               <span class="input-group-text">=</span>
-              <input type="text" class="form-control" :value="krwAmount" readonly />
+              <input
+                type="text"
+                class="form-control"
+                :value="krwAmount"
+                readonly
+              />
               <div class="input-group-append">
                 <span class="input-group-text">KRW</span>
               </div>
@@ -39,13 +49,26 @@
               <canvas id="krwToUsdChart"></canvas>
             </div>
             <div class="input-group my-3">
-              <input type="number" class="form-control" v-model.number="krwAmountReverse" @input="convertToUsd" />
+              <input
+                type="number"
+                class="form-control"
+                v-model.number="krwAmountReverse"
+                @input="convertToUsd"
+              />
               <div class="input-group-append">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/0/09/Flag_of_South_Korea.svg" alt="한국 국기"
-                  class="flag-icon" />
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/0/09/Flag_of_South_Korea.svg"
+                  alt="한국 국기"
+                  class="flag-icon"
+                />
               </div>
               <span class="input-group-text">=</span>
-              <input type="number" class="form-control" :value="usdAmountReverse" readonly />
+              <input
+                type="number"
+                class="form-control"
+                :value="usdAmountReverse"
+                readonly
+              />
               <div class="input-group-append">
                 <span class="input-group-text">USD</span>
               </div>
@@ -61,21 +84,39 @@
       <div class="col-lg-12">
         <div class="card">
           <div class="card-body">
-            <div @click="$router.push('/set-alert')" class="alert alert-info clickable-alert" role="button">
+            <div
+              @click="$router.push('/set-alert')"
+              class="alert alert-info clickable-alert"
+              role="button"
+            >
               <span>Set an alert for exchange rates</span>
             </div>
             <div class="form-group">
               <label for="autoCondition">Auto Condition</label>
-              <input type="text" class="form-control" id="autoCondition"
-                value="Target Exchange: 1,330 KRW. Current rate: 1,000,000 KRW = 90 USD" />
+              <input
+                type="text"
+                class="form-control"
+                id="autoCondition"
+                value="Target Exchange: 1,330 KRW. Current rate: 1,000,000 KRW = 90 USD"
+              />
             </div>
             <div class="form-group">
               <label for="targetRate1">Target Rate 1</label>
-              <input type="text" class="form-control" id="targetRate1" value="1,330 KRW" />
+              <input
+                type="text"
+                class="form-control"
+                id="targetRate1"
+                value="1,330 KRW"
+              />
             </div>
             <div class="form-group">
               <label for="targetRate2">Target Rate 2</label>
-              <input type="text" class="form-control" id="targetRate2" value="1,329 KRW" />
+              <input
+                type="text"
+                class="form-control"
+                id="targetRate2"
+                value="1,329 KRW"
+              />
             </div>
           </div>
         </div>
@@ -89,8 +130,8 @@ import { ref, onMounted, watch } from "vue";
 import { useExchangeStore } from "@/stores/exchangeStore";
 
 // API URL과 API 키를 설정합니다.
-const usdToKrwUrl = `exchange/v6/${import.meta.env.VITE_EXCHANGE_API_KEY}/pair/USD/KRW`;
-const krwToUsdUrl = `exchange/v6/${import.meta.env.VITE_EXCHANGE_API_KEY}/pair/KRW/USD`;
+const usdToKrwUrl = "/exchange/v6/6bbbf78cc42a296d533a9e6b/pair/USD/KRW";
+const krwToUsdUrl = "/exchange/v6/6bbbf78cc42a296d533a9e6b/pair/KRW/USD";
 
 // 데이터 변수
 const usdAmount = ref(1); // 초기값을 1로 설정
@@ -165,11 +206,8 @@ onMounted(() => {
 }
 
 .flag-icon {
-  width: 50px;
-  /* 적절한 너비로 설정 */
-  height: auto;
-  /* 자동 높이 조정 */
-  vertical-align: middle;
-  /* 텍스트와 이미지 정렬 맞추기 */
+  width: 50px; /* 적절한 너비로 설정 */
+  height: auto; /* 자동 높이 조정 */
+  vertical-align: middle; /* 텍스트와 이미지 정렬 맞추기 */
 }
 </style>
