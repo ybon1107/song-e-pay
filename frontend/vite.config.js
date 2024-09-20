ba29ea28adcf816066fbe8a5608f162b0e293617
ssooomm <fivesoomin@gmail.com>
2024-09-20 10:26:40 +0900

    환경변수로 변경

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,55 +1,60 @@
-import { defineConfig } from "vite";
+import { defineConfig,loadEnv } from "vite";
 import path from "path";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import envCompatible from "vite-plugin-env-compatible";
 import { createHtmlPlugin } from "vite-plugin-html";
 import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    __VUE_OPTIONS_API__: true, // 필요한 플래그 설정
-    __VUE_PROD_DEVTOOLS__: false,
-  },
-  plugins: [
-    vue(),
-    vueJsx(),
-    viteCommonjs(),
-    envCompatible(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          title: "frontend",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return{
+    define: {
+      __VUE_OPTIONS_API__: true, // 필요한 플래그 설정
+      __VUE_PROD_DEVTOOLS__: false,
+    },
+    plugins: [
+      vue(),
+      vueJsx(),
+      viteCommonjs(),
+      envCompatible(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: "frontend",
+          },
+        },
+      }),
+    ],
+    resolve: {
+      alias: [
+        {
+          find: /^~/,
+          replacement: "",
+        },
+        {
+          find: "@",
+          replacement: path.resolve(__dirname, "src"),
+        },
+      ],
+      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_TARGET,
+          changeOrigin: true,
+        },
+        "/exchange": {
+          target: env.VITE_EXCHANGE_API_TARGET,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/exchange/, ""),
         },
       },
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /^~/,
-        replacement: "",
-      },
-      {
-        find: "@",
-        replacement: path.resolve(__dirname, "src"),
-      },
-    ],
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://localhost:8080",
-        changeOrigin: true,
-      },
-      "/exchange": {
-        target: "https://v6.exchangerate-api.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/exchange/, ""),
-      },
     },
-  },
-  build: {},
+    build: {},
+  }
+  
 });
+
