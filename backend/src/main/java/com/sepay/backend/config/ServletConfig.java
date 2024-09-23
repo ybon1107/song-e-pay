acae0267977e91d04d326d565ca0ff433a12c187
ddubuk228 <practice2280@gmail.com>
2024-09-23 17:47:24 +0900

    origin dev pull

--- backend/src/main/java/com/sepay/backend/config/ServletConfig.java
+++ backend/src/main/java/com/sepay/backend/config/ServletConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
