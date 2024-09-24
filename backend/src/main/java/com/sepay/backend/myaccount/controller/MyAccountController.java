ed73808681bd9b3bd6ca16b7d8e269c27a83d1e4
cyh000127 <okccc5@naver.com>
2024-09-24 16:17:43 +0900

    fix: 모달

--- backend/src/main/java/com/sepay/backend/myaccount/controller/MyAccountController.java
+++ backend/src/main/java/com/sepay/backend/myaccount/controller/MyAccountController.java
@@ -1,16 +1,14 @@
 package com.sepay.backend.myaccount.controller;
 
 import com.sepay.backend.myaccount.dto.AccountDTO;
+import com.sepay.backend.myaccount.dto.DTORequest;
 import com.sepay.backend.myaccount.dto.KrwAccountDTO;
 import com.sepay.backend.myaccount.dto.SongAccountDTO;
 import com.sepay.backend.myaccount.service.MyAccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -20,33 +18,44 @@ public class MyAccountController {
 
     final MyAccountService myAccountService;
 
-    //송이 계좌 충전
+    //송이 계좌 잔액 조회
+    @PostMapping("/krwbalance")
+    public ResponseEntity<?> getKrwAccountBalance(@RequestParam String krwNo) {
+        return ResponseEntity.ok(myAccountService.selectKrwBalance(krwNo));
+    }
+    //원화 계좌 잔액 조회
+    @PostMapping("/songebalance")
+    public ResponseEntity<?> getSongAccountBalance(@RequestParam String songNo) {
+        return ResponseEntity.ok(myAccountService.selectSongBalance(songNo));
+    }
+    // 충전
     @PostMapping("/deposit")
-    public ResponseEntity<?> deposit(@RequestParam double amount, AccountDTO accountDTO, SongAccountDTO songAccountDTO) {
-        return ResponseEntity.ok(myAccountService.deposit(accountDTO, songAccountDTO, amount));
+    public ResponseEntity<?> deposit(@RequestBody DTORequest request) {
+        // DepositRequest 클래스를 사용하여 accountDTO, songAccountDTO 및 amount를 받습니다.
+        return ResponseEntity.ok(myAccountService.deposit(request.getAccountDTO(), request.getSongAccountDTO(), request.getHistoryDTO(), request.getAmount()));
     }
 
     // 환불
     @PostMapping("/refund")
-    public ResponseEntity<?> refund(@RequestParam double amount, AccountDTO accountDTO, SongAccountDTO songAccountDTO) {
-        return ResponseEntity.ok(myAccountService.refund(accountDTO, songAccountDTO, amount));
+    public ResponseEntity<?> refund(@RequestBody DTORequest request) {
+        return ResponseEntity.ok(myAccountService.refund(request.getAccountDTO(), request.getSongAccountDTO(), request.getHistoryDTO(), request.getAmount()));
     }
 
     // 환전
     @PostMapping("/exchange")
-    public ResponseEntity<?> exchange(@RequestParam double amount, double exchangeRate, SongAccountDTO songAccountDTO, KrwAccountDTO krwAccountDTO) {
-        return ResponseEntity.ok(myAccountService.exchange(songAccountDTO, krwAccountDTO , amount, exchangeRate));
+    public ResponseEntity<?> exchange(@RequestBody DTORequest request) {
+        return ResponseEntity.ok(myAccountService.exchange(request.getSongAccountDTO(), request.getKrwAccountDTO(), request.getHistoryDTO() , request.getAmount(), request.getExchangeRate()));
     }
 
     // 환급
     @PostMapping("/re-exchange")
-    public ResponseEntity<?> reExchange(@RequestParam double amount, double exchangeRate, SongAccountDTO songAccountDTO, KrwAccountDTO krwAccountDTO) {
-        return ResponseEntity.ok(myAccountService.reExchange(songAccountDTO, krwAccountDTO , amount, exchangeRate));
+    public ResponseEntity<?> reExchange(@RequestBody DTORequest request) {
+        return ResponseEntity.ok(myAccountService.reExchange(request.getSongAccountDTO(), request.getKrwAccountDTO(), request.getHistoryDTO() , request.getAmount(), request.getExchangeRate()));
     }
 
     // 송금
     @PostMapping("/transfer")
-    public ResponseEntity<?> transfer(@RequestParam double amount, KrwAccountDTO myKrwAccount, String target_krwNo) {
-        return ResponseEntity.ok(myAccountService.transfer(myKrwAccount, amount, target_krwNo));
+    public ResponseEntity<?> transfer(@RequestBody DTORequest request) {
+        return ResponseEntity.ok(myAccountService.transfer(request.getKrwAccountDTO(), request.getHistoryDTO(), request.getAmount(), request.getTarget_krwNo()));
     }
 }
