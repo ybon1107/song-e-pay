679c5999f71b28bf769fea9d766cded9b8790a3e
ssooomm <fivesoomin@gmail.com>
2024-09-24 10:32:55 +0900

    배경 초록색 제거

--- frontend/src/components/sidenav/index.vue
+++ frontend/src/components/sidenav/index.vue
@@ -2,7 +2,7 @@
 import { computed } from "vue";
 import { useStore } from "vuex";
 import SidenavList from "./SidenavList.vue";
-import logo from "@/assets/img/logo-ct-dark.png";
+import logo from "@/assets/img/songepay_logo.png";
 import logoWhite from "@/assets/img/logo-ct.png";
 
 const store = useStore();
@@ -12,11 +12,12 @@ const sidebarType = computed(() => store.state.sidebarType);
 const darkMode = computed(() => store.state.darkMode);
 </script>
 <template>
-  <div
+  <!-- 기존 초록색 배경 부분 -->
+  <!-- <div
     v-show="layout === 'default'"
     class="min-height-300 position-absolute w-100"
     :class="`${darkMode ? 'bg-transparent' : 'bg-success'}`"
-  />
+  /> -->
 
   <aside
     class="my-3 overflow-auto border-0 sidenav navbar navbar-vertical navbar-expand-xs border-radius-xl"
