3e02f34c4b1d7fcf50c2468df7e54325acf0059f
yesslee31 <yesslee31@gmail.com>
2024-09-26 14:33:19 +0900

    fix: 회원가입 pinia store 파일명 수정, 코드 포맷 변경, 세션 스토리지 저장 방식으로 수정

---
+++ frontend/src/stores/signupStore.js
@@ -0,0 +1,32 @@
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+
+export const useSignupStore = defineStore('signup', () => {
+  const email = ref(sessionStorage.getItem('email') || '');
+  const countryCallingCode = ref(sessionStorage.getItem('countryCallingCode') || '');
+  const phoneNumber = ref(sessionStorage.getItem('phoneNumber') || '');
+
+  const setEmail = (newEmail) => {
+    email.value = newEmail;
+    sessionStorage.setItem('email', newEmail);
+  };
+
+  const setCountryCallingCode = (newCountryCallingCode) => {
+    countryCallingCode.value = newCountryCallingCode;
+    sessionStorage.setItem('countryCallingCode', newCountryCallingCode);
+  };
+
+  const setPhoneNumber = (newPhoneNumber) => {
+    phoneNumber.value = newPhoneNumber;
+    sessionStorage.setItem('phoneNumber', newPhoneNumber);
+  };
+
+  return {
+    email,
+    countryCallingCode,
+    phoneNumber,
+    setEmail,
+    setCountryCallingCode,
+    setPhoneNumber,
+  };
+});
