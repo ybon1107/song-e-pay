edcc1650cd1d78918b817215dae475cc40991968
yesslee31 <yesslee31@gmail.com>
2024-10-01 16:45:28 +0900

    update: 이메일 인증코드 생성 및 발송 로직 구현

--- backend/src/main/java/com/sepay/backend/mail/controller/MailController.java
+++ backend/src/main/java/com/sepay/backend/mail/controller/MailController.java
@@ -1,16 +1,14 @@
 package com.sepay.backend.mail.controller;
 
-import com.sepay.backend.mail.dto.MailDTO;
 import com.sepay.backend.mail.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api/register/email")
+@RequestMapping("/api/register")
 public class MailController {
     private final MailService mailService;
 
@@ -19,15 +17,14 @@ public class MailController {
         this.mailService = mailService;
     }
 
-    @PostMapping("/check")
-    public void sendMail() throws Exception {
-        // 프론트엔드에서 입력받은 값 가져와서 메일 보내기
-        String subject = "회원가입 인증 메일입니다."; // 메일 제목
-        String body = "회원가입을 위한 인증 메일입니다. 인증 코드를 입력하여 회원가입을 완료해주세요.";
-        String to = "yesslee31@gmail.com"; // 입력받은 user의 이메일 가져와서 넣기
-        String from = "noreplysongepay@accounts.google.com";
-
-        MailDTO mailDTO = new MailDTO(to, from, subject, body);
-        mailService.sendEmail(mailDTO);
+    @PostMapping("/email")
+    public boolean sendMail(@RequestBody Map<String, String> request) throws Exception {
+        String userId = request.get("userId");
+        return mailService.sendEmail(userId);
     }
+
+//    @PostMapping("/check")
+//    public boolean checkCode(String code) {
+//        return mailService.checkCode(code);
+//    }
 }
--- backend/src/main/java/com/sepay/backend/mail/service/MailServiceImpl.java
+++ backend/src/main/java/com/sepay/backend/mail/service/MailServiceImpl.java
@@ -1,6 +1,5 @@
 package com.sepay.backend.mail.service;
 
-import com.sepay.backend.mail.dto.MailDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -23,29 +22,57 @@ public class MailServiceImpl implements MailService {
     }
 
     @Override
-    public void sendEmail(MailDTO mailDTO) {
+    public String createVerificationCode() {
+        StringBuilder code = new StringBuilder();
+
+        for (int i = 0; i < 10; i++) {
+            int random = (int) (Math.random() * 3); // 0: 숫자, 1,2: 알파벳
+
+            if (random == 0) {
+                code.append((int) (Math.random() * 10));
+            } else {
+                char alphabet = (char) ((int) (Math.random() * 26) + 65);
+                int upperOrLower = (int) (Math.random() * 2); // 0: 대문자, 1: 소문자
+                if (upperOrLower == 0) {
+                    code.append(alphabet);
+                } else {
+                    code.append(Character.toLowerCase(alphabet));
+                }
+            }
+        }
+        return code.toString();
+    }
+
+    @Override
+    public boolean sendEmail (String userId){
+        // 이메일 인증 코드 생성
+        String verifyCode = createVerificationCode();
+
         try {
             MimeMessage message = emailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
             helper.setFrom("noreplysongepay@account.google.com");
-            helper.setTo(mailDTO.getTo());
-            helper.setSubject(mailDTO.getSubject());
-            helper.setText(mailDTO.getBody(), true); // true를 설정해서 HTML을 사용 가능하게 함
+            helper.setTo(userId);
+            helper.setSubject("[Song-E Pay] Registration Verification Email");
+            String body = "<h2>Your verification code is: </h2><h3>" + verifyCode + "</h3>";
+            helper.setText(body, true); // true를 설정해서 HTML을 사용 가능하게 함
             helper.setReplyTo("noreplysongepay@account.google.com"); // 회신 불가능한 메일 주소 설정
 
             emailSender.send(message);
-            log.info("Mail sent successfully: {}", mailDTO.getBody());
+            log.info("Mail sent successfully: {}", userId);
+
         } catch (MessagingException e) {
             log.error("Error occurred while sending mail", e);
-            throw new RuntimeException(e);
+
+            return false;
         }
+        return true;
     }
 
-    public SimpleMailMessage createEmailForm(MailDTO mailDTO) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(mailDTO.getTo());
-        message.setSubject(mailDTO.getSubject());
-        message.setText(mailDTO.getBody());
-        return message;
-    }
+//    @Override
+//    public boolean checkCode(String code) {
+//        // 인증 코드 검증 로직
+//
+//        return true;
+//    }
 }
