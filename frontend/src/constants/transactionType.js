f5374d92bbd6dc32df975e9a25ef2cd9de5dbde6
ssooomm <fivesoomin@gmail.com>
2024-10-05 16:24:02 +0900

    거래명 키값으로 변경

--- frontend/src/constants/transactionType.js
+++ frontend/src/constants/transactionType.js
@@ -7,14 +7,15 @@ export const TRANSACTION_TYPES = {
     RE_EXCHANGE: 6,
 };
 
-export const TRANSACTION_TYPES_KEY = {
-    PAYMENT: 'payment',
-    TRANSFER: 'transfser',
-    DEPOSIT: 'deposit',
-    REFUND: 'refund',
-    EXCHANGE: 'exchange',
-    RE_EXCHANGE: 're_exchange',
-};
+export const TRANSACTION_TYPES_KEY = [
+    'none',
+    'transaction_types_payment',
+    'transaction_types_transfser',
+    'transaction_types_deposit',
+    'transaction_types_refund',
+    'transaction_types_exchange',
+    'transaction_types_reExchange',
+];
 
 //historiesmodal에서 사용중, 추후에 제거해야함 
 export const TRANSACTION_TYPES_STRING_KO = {
@@ -25,13 +26,3 @@ export const TRANSACTION_TYPES_STRING_KO = {
     EXCHANGE: '환전',
     RE_EXCHANGE: '환급',
 };
-
-//   export const TRANSACTION_TYPES_REVERSE = {
-//     1: 'Payment',
-//     2: 'Transfer',
-//     3: 'Deposit',
-//     4: 'Refund',
-//     5: 'Exchange',
-//     6: 'RE_EXCHANGE',
-//   };
-
