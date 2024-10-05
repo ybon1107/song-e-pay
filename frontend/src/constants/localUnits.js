d62f914836affc7ab7727530a882d359a4a73072
ssooomm <fivesoomin@gmail.com>
2024-10-05 15:27:30 +0900

    i18n추가

--- frontend/src/constants/localUnits.js
+++ frontend/src/constants/localUnits.js
@@ -1,21 +1,21 @@
 export const PROVINCES = [
-    '서울특별시',
-    '부산광역시',
-    '대구광역시',
-    '인천광역시',
-    '광주광역시',
-    '대전광역시',
-    '울산광역시',
-    '세종특별자치시',
-    '경기도',
-    '강원도',
-    '충청북도',
-    '충청남도',
-    '전라북도',
-    '전라남도',
-    '경상북도',
-    '경상남도',
-    '제주특별자치도',
+    'provinces--seoul',
+    'provinces--busan',
+    'provinces--daegu',
+    'provinces--incheon',
+    'provinces--gwangju',
+    'provinces--daejeon',
+    'provinces--ulsan',
+    'provinces--sejong',
+    'provinces--gyeonggi',
+    'provinces--gangwon',
+    'provinces--chungcheongbuk',
+    'provinces--chungcheongnam',
+    'provinces--jeollabuk',
+    'provinces--jeollanam',
+    'provinces--gyeongsangbuk',
+    'provinces--gyeongsangnam',
+    'provinces--jeju'
 ];
 
 export const SEOUL_CITIES = [
@@ -262,3 +262,4 @@ export const GYEONGSANGNAM_CITIES = [
     '합천군',
 ];
 export const JEJU_CITIES = ['서귀포시', '제주시'];
+
