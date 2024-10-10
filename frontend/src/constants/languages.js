d90d84b20bd5045a70fd2cdce48fa1d67a32ed38
M6F\student <qhdbs0802@naver.com>
2024-10-10 11:14:56 +0900

    언어파일변경

--- frontend/src/constants/languages.js
+++ frontend/src/constants/languages.js
@@ -1,6 +1,6 @@
 export const languages = [
-    { code: 'ko', name: '한국어', flag: '/src/assets/img/icons/flags/kr.png' },
-    { code: 'en', name: 'English', flag: '/src/assets/img/icons/flags/us.png' },
-    { code: 'id', name: 'Indonesia', flag: '/src/assets/img/icons/flags/id.png' },
-    { code: 'vn', name: 'Vietnam', flag: '/src/assets/img/icons/flags/vn.png' },
-  ];
+  { code: "ko", name: "한국어", flag: "/src/assets/img/icons/flags/kr.png" },
+  { code: "en", name: "English", flag: "/src/assets/img/icons/flags/us.png" },
+  { code: "id", name: "Indonesia", flag: "/src/assets/img/icons/flags/id.png" },
+  { code: "vi", name: "Vietnam", flag: "/src/assets/img/icons/flags/vn.png" },
+];
