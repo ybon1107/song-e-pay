439ac8152e48e99279c0ee65caa6318882e48935
ssooomm <fivesoomin@gmail.com>
2024-10-11 11:32:09 +0900

    selected옵션 추가

--- frontend/src/views/Cards/AccountsCard.vue
+++ frontend/src/views/Cards/AccountsCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="user">
-    <div class="card card-img-bg" :style="{ backgroundImage: `url(${backgroundImage})` }">
+    <div class="card card-img-bg" :id="props.assetType === 'song-e' ? 'card-songe' : 'card-wone'" :style="{ backgroundImage: `url(${backgroundImage})` }" :class="props.assetType === 'song-e' ? 'bg-primary' : 'bg-info'">
       <div class="card-body d-flex align-items-end justify-content-end">
         <div class="d-flex align-items-center">
           <div class="icon-container me-2">
@@ -13,14 +13,14 @@
     </div>
   </div>
   <div v-else>
-    <div class="card card-img-bg" :style="{ bacgroundColor: gray }"></div>
+    <div class="card card-img-bg" :style="{ backgroundColor: gray }"></div>
   </div>
 </template>
 
 <script setup>
-import { defineProps, ref, computed, onMounted,watchEffect } from 'vue';
+import { defineProps, ref, computed,watchEffect } from 'vue';
 import myaccountApi from '../../api/myaccountApi';
-import { INTL_LOCALE, CURRENCY_NAME } from '@/constants/countryCode';
+import { CURRENCY_NAME } from '@/constants/countryCode';
 
 import currencyFormatter from '../../js/currencyFormatter';
 const { formatNumber } = currencyFormatter;
@@ -28,7 +28,6 @@ const { formatNumber } = currencyFormatter;
 import { useAuthStore } from '@/stores/auth';
 const auth = useAuthStore();
 const user = computed(() => auth.user);
-const isFocused = ref(false);
 const balance = ref(0);
 
 const props = defineProps({
@@ -40,9 +39,10 @@ const props = defineProps({
 
 //추후에 i18n키값 구분 로직으로 변경
 const backgroundImage = computed(() => `/images/${props.assetType}-money.png`);
+
 const flagIcon = computed(() => {
   const iconName = props.assetType === 'song-e' ? user.value?.countryCode : '0';
-  return `/images/flag_${iconName}.png`;
+  return `/images/flag_c${iconName}.png`;
 });
 const displayCountry = computed(() => {
   return props.assetType === 'song-e' ? CURRENCY_NAME[user.value?.countryCode] : CURRENCY_NAME[0];
@@ -82,6 +82,20 @@ defineExpose({ fetchBalance });
 </script>
 
 <style scoped>
+ /* acountscard가 선택됐을 때 */
+ .selected #card-songe {
+    border: 3px solid #FFD166; 
+   box-shadow: 0 0 30px #FFD166;
+   transform: scale(1.08);
+   transition: all 0.3s ease;
+ }
+ .selected #card-wone {
+    border: 3px solid #5C9EA6 ;
+   box-shadow: 0 0 30px #5C9EA6 ;
+   transform: scale(1.08);
+   transition: all 0.3s ease;
+ }
+
 @media (max-width: 500px) {
   .card {
     width: 90%;
