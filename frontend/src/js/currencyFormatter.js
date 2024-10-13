c04cd8078b5115a4091113d501beb58771b44b1c
ssooomm <fivesoomin@gmail.com>
2024-10-14 04:09:40 +0900

    작업 중

--- frontend/src/js/currencyFormatter.js
+++ frontend/src/js/currencyFormatter.js
@@ -1,4 +1,5 @@
 
+import { COUNTRY_CODE } from '@/constants/countryCode';
 
 
 // 숫자 포맷팅 함수 추가
@@ -7,17 +8,36 @@ const formatNumber = (num) => {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
+const reFormatNumber = (num) => {
+    if (num !== null) {
+        return num.toString().replace(/[^\d.]/g, '');
+    }
+    return num;
+}
+
+const exchangeCurrency = (amount, rate) => {
+    // const num = reFormatNumber(amount);
+    return Math.round(amount * rate);
+}
+// const exchangeCurrencyRevert = (amount, rate) => {
+//     console.log(amount, rate);
+//     console.log(Math.floor(amount / rate));
+//     return Math.floor(amount / rate);
+// }
+
 // 통화 포맷팅 함수
-const formatCurrency = (value, locale, currency) => {
-    return new Intl.NumberFormat(locale, {
-        style: 'currency',
-        currency: currency,
-        currencyDisplay: 'code'
-    }).format(value);
-};
+// const formatCurrency = (value, locale, currency) => {
+//     return new Intl.NumberFormat(locale, {
+//         style: 'currency',
+//         currency: currency,
+//         currencyDisplay: 'code'
+//     }).format(value);
+// };
 
 
 export default {
     formatNumber,
-    formatCurrency
+    reFormatNumber,
+    exchangeCurrency,
+    // exchangeCurrencyRevert
 }
