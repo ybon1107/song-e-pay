c416b8a14a742cd73be42df1ad4379494ca0bfe3
yesslee31 <yesslee31@gmail.com>
2024-10-14 17:26:22 +0900

    fix: 토큰 만료시 로그아웃 처리 기능 수정

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/stores/auth';
 import router from '@/router';
 
-const instance = axios.create({
+const api = axios.create({
   timeout: 10000,
   withCredentials: true,
   headers: {
@@ -12,16 +12,13 @@ const instance = axios.create({
 });
 
 // 요청 인터셉터
-instance.interceptors.request.use(
+api.interceptors.request.use(
   (config) => {
     console.log("Request Interceptor Called");
-    // 토큰이 있는 경우 헤더에 추가
     const authStore = useAuthStore();
     const token = authStore.getToken();
-    console.log("Token in interceptor: " + token);
-    // const { getToken } = useAuthStore();
-    // const token = getToken();
-    if (token) {
+
+    if (token) { // 토큰이 존재하면 헤더에 추가
       config.headers.Authorization = `Bearer ${token}`;
       // config.headers['Authorization'] = `Bearer ${token}`;
       console.log("Header Info: " + config.headers.Authorization);
@@ -36,29 +33,41 @@ instance.interceptors.request.use(
 );
 
 // 응답 인터셉터
-instance.interceptors.response.use(
+api.interceptors.response.use(
   (response) => {
     if (response.status === 200) {
       return response;
     }
     if (response.status === 404) {
+      router.push('/error');
       return Promise.reject('404 Not Found: ' + response.request);
     }
     return response;
   },
   (error) => {
+    const authStore = useAuthStore();
+
     // 응답 에러 처리
     if (error.response?.status === 401) {
-      const { logout } = useAuthStore();
-      logout();
-      // 인증 오류인 경우 로그인 페이지로 이동
-      router.push('/login');
-    } else if (error.response?.status === 403) {
-      // 권한 오류인 경우 메인 페이지로 이동
-      router.push('/');
+      const contentType = error.response.headers['content-type'];
+
+      if (contentType && contentType.includes('text/html')) {
+        const userConfirmed = confirm('세션이 만료되었습니다. 다시 로그인하시겠습니까?');
+
+        if (userConfirmed) {
+          authStore.logout();
+          router.push('/login');
+        } else {
+          authStore.logout();
+          router.push('/main');
+        }
+      } else if (error.response?.status === 403) {
+        // 권한 오류인 경우 메인 페이지로 이동
+        router.push('/');
+      }
+      return Promise.reject(error);
     }
-    return Promise.reject(error);
   }
 );
 
-export default instance;
+export default api;
