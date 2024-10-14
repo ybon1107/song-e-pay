bed376bb058785dd6c21d9b3c6e8d1f0014c7aa0
yesslee31 <yesslee31@gmail.com>
2024-10-14 17:21:51 +0900

    refactor: 중복 인터셉터 코드 개선

--- frontend/src/api/settingApi.js
+++ frontend/src/api/settingApi.js
@@ -1,28 +1,12 @@
-import axios from "axios";
+import api from "@/api"
 import { useAuthStore } from "@/stores/auth";
 
-const api = axios.create({
-  baseURL: "/api/setting",
-  headers: { "Content-Type": "application/json" },
-});
-
-api.interceptors.request.use((config) => {
-  const authStore = useAuthStore();
-  const token = authStore.getToken();
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+const BASE_URL = "/api/setting";
+const headers = { "Content-Type": "multipart/form-data" };
 
 export default {
   async updateProfile(userInfo) {
     console.log("userInfo: ", userInfo);
-    let headers = { "Content-Type": "multipart/form-data" };
 
     const formData = new FormData();
 
@@ -36,7 +20,7 @@ export default {
       console.log("profilePic: ", userInfo.profilePic);
     }
 
-    const { data } = await api.post("/", formData, { headers });
+    const { data } = await api.post(`${BASE_URL}`, formData, headers);
     console.log("updateProfile: ", data);
 
     const auth = useAuthStore();
@@ -47,22 +31,21 @@ export default {
 
   async changePassword(formData) {
     console.log("formData: ", formData);
-    const { data } = await api.patch("/change-password", formData);
+    const { data } = await api.patch(`${BASE_URL}/change-password`, formData);
     console.log("AUTH PUT: ", data);
     return data;
   },
 
   async delete(userId) {
     console.log("userId: ", userId);
-    const { data } = await api.delete(`/bye/${userId}`);
+    const { data } = await api.delete(`${BASE_URL}/bye/${userId}`);
     console.log("delete: ", data);
     window.location.href = "/";
   },
 
-
   async submitSecPwd(formData) {
     try {
-      const response = await api.post('/check-secpwd', formData);
+      const response = await api.post(`${BASE_URL}/check-secpwd`, formData);
       return response;
     } catch (error) {
       console.error('비밀번호 제출 오류:', error.response.data);
